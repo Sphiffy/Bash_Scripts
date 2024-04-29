ffc2d1ea9c26a370f60939609709ba29029c2214
Sphiffy <aitzazDX@yahoo.com>
2024-04-29 17:06:59 -0400

    Further expanding comments

--- network_report.sh
+++ network_report.sh
@@ -5,11 +5,18 @@
 # Description: Generate a report that gathers the systems current network configuration and statistics
 
 
+
 # Variables
+#--------------------------------------------------------------------------------------------------
+# System Hostname
 system_hostname=$(hostname)
+# System Distribution
 system_distribution=$(cat /etc/redhat-release)
+# System Kernel Version
 system_kernel_ver=$(uname -r)
+# System Network Interface Card information, tr -s replaces all spaces with one blankspace character, ark with delimiter blankspace shows fields 1, 2, and 3
 system_nics=$(ip -br addr | grep -v lo | tr -s ' ' | awk -F ' ' '{ print$1,$2,$3}')
+# Network Interface Names
 interfaces=$( nmcli con show | tail -n+2 | awk '{print $1}')
 
 # Create the report file with the current date
@@ -31,8 +38,7 @@ report_header() {
   echo -e "----------------------------------------\n"
 }
 
-# Configure information to dump into report
-
+# Configure information to dump into report using fuction
 dump_info() {
   echo -e "Network Report For: $system_hostname" > $REPORT
   echo -e "----------------------------------------\n" >> $REPORT
