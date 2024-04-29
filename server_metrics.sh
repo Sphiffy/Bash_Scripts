f8d4e4754a638734c6cee756c1426ee50361f41e
Sphiffy <aitzazDX@yahoo.com>
2024-04-29 10:26:06 -0400

    draft 1, adding description, system uptime and load averages

---
+++ server_metrics.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Author Sphiffy
+# Date: 4/29/2024
+# Description: This script is used to gather differennt system metrics from linux based hosts
+
+
+# Creating variable to store system hostname
+hostname=$(hostname)
+echo -e "\nSystem Metrics for $hostname"
+echo -e "------------------------------------------------------------\n"
+
+# Creating variable to gather current uptime using awk to get only time related fields, awk again to remove ending comma from the string
+system_uptime=$(uptime | awk -F ' ' ' {print $2,$3,$4}' | awk '$0=gensub(/^(.*),/,"\\1 ","g",$0)')
+echo -e "System Uptime: $system_uptime"
+
+# Creating variable to gather current load averages from uptime, use tr to translate all blankspaces to single blackspaces, use cut to remove all fields except 7 to the end using blankspace as the delimiter
+system_load=$(uptime | tr -s ' ' | cut -d ' ' -f 7-)
+echo -e "System Load: $system_load"
