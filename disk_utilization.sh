a084c6d1fb3375f6210c2ad3d7a8671a4cb87953
Sphiffy <aitzazDX@yahoo.com>
2024-04-29 12:01:31 -0400

    Edited comments

--- disk_utilization.sh
+++ disk_utilization.sh
@@ -9,6 +9,7 @@
 partitions=$(df -h | grep -v tmpfs | grep -v Filesystem | tr -s ' ' | awk -F ' ' '{print $1}')
 
 # Command to get disk size: `df -h | grep /boot | tr -s ' ' | awk -F ' ' '{print $5}' | awk '{ print substr( $0, 1, length($0)-1 ) }'`
+# Awk command used to remove last character from string, in this case used to remove the % sign `awk '{ print substr( $0, 1, length($0)-1 ) }'`
 
 # Create a for loop that goes through each partition and checks if the disk size is greater than 80 percent utilization, the loop uses each partition, and the if statement checks if it's value is greater than or equal to 80
 for x in $partitions; do
