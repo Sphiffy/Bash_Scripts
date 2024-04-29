32c30e65e1ea164c776bd173c5fca8aeb4d563c1
Sphiffy <aitzazDX@yahoo.com>
2024-04-29 09:59:51 -0400

    Adding Directory and File type scripts

---
+++ directory_size.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Take input from the user running the script using the read command and -p for prompt
+read -p 'Enter directory absolute path: ' dir_name
+
+# If conditional statement to check if the user input matches an existing directoy, if so create a variable called dir_size that uses du to get its size and awk to print only the first column
+if [[ -d $dir_name ]]; then
+  dir_size=$(du -sh /home | awk '{print $1}')
+  echo "The $dir_name is $dir_size"
+
+# If the user input is not a directory but a file return the message that the input was not a directory
+elif [[ -e $dir_name ]]; then
+  echo "$dir_name is not a directory"
+
+# If the user input was neither a file or directory return that it cannot find the inputed path
+else
+  echo "Unable to find $dir_name"
+fi
---
+++ file_type.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Ask user to input in the promt the aboslute path for a file and save it in the variable file_name
+read -p "Enter the absolute path to the file or directory: " file_name
+
+# Use an if statement to first check if the input exists, then use the stat command with awk to get the file type, [[ -e xxx ]] checks if xxx exists and if not exit the script
+if [[ ! -e $file_name ]]; then
+  echo "$file_name not found. Exiting"
+  exit
+
+# If the file exists create a variable called file_type and store the type from the stat command with string manipulation to get just the file type in the variable
+else
+  file_type=$(stat $file_name | awk -F ' ' 'NR==2' | tr -s ' ' | cut -d ' ' -f 9-)
+fi
+
+# Use a case statement to run through different code blocks depending on file type, display the file type and then the total size
+case "$file_type" in
+  "regular file")
+    echo -e "$file_name is a $file_type\nSize: $(du -sh $file_name | awk -F ' ' '{print $1}')"
+  ;;
+  "directory")
+    echo -e "$file_name is a $file_type\nSize: $(du -sh $file_name | awk -F ' ' '{print $1}')"
+  ;;
+  "character special file")
+    echo -e "$file_name is a $file_type\nSize: $(du -sh $file_name | awk -F ' ' '{print $1}')"
+  ;;
+  "symbolic link")
+    echo -e "$file_name is a $file_type\nSize: $(du -sh $file_name | awk -F ' ' '{print $1}')"
+  ;;
+  "block special file")
+    echo -e "$file_name is a $file_type\nSize: $(du -sh $file_name | awk -F ' ' '{print $1}')"
+  ;;
+  *)
+    echo "No available type in script"
+  ;;
+esac
+
+# Testing a new line of comment code
