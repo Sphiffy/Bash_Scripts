7a5befc68859ca9fc7af5988cc8df4cc5df4bdbe
Sphiffy <aitzazDX@yahoo.com>
2024-04-29 12:19:37 -0400

    Adding root check

--- network_report.sh
+++ network_report.sh
@@ -5,4 +5,16 @@
 # Description: Generate a report that gathers the systems current network configuration and statistics
 
 
-#
+# Create the report file with the current date
+REPORT="network.$(date +'%d-%m-%y').info.txt"
+
+# Configure a root check to ensure non root users are unable to run the script, the local variable keeps the variable inside this function block only
+chk_root() {
+  local user_id=$(id -u)
+  if [[ $user_id -ne 0 ]]; then
+    echo -e "You must be the root user to run this tool"
+    exit 999
+  fi
+}
+
+chk_root
