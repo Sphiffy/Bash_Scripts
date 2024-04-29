4f5719f29f6b73f6497843712876b23d22bb0ef3
Sphiffy <aitzazDX@yahoo.com>
2024-04-29 11:57:23 -0400

    Added partition size check block

--- disk_utilization.sh
+++ disk_utilization.sh
@@ -5,4 +5,16 @@
 # Description: Run through all the current disk partitions on the system and report any that exceed an 80% threshold
 
 
-# 
+# Create a varaible that gets the list of all disk partitions on the system excluding any temporary filesystems
+partitions=$(df -h | grep -v tmpfs | grep -v Filesystem | tr -s ' ' | awk -F ' ' '{print $1}')
+
+# Command to get disk size: `df -h | grep /boot | tr -s ' ' | awk -F ' ' '{print $5}' | awk '{ print substr( $0, 1, length($0)-1 ) }'`
+
+
+# Create a for loop that goes through each partition and checks if the disk size is greater than 80 percent utilization, the loop uses each partition, and the if statement checks if it's value is greater than or equal to 80
+for x in $partitions; do
+  if [[ $(df -h | grep $x | tr -s ' ' | awk -F ' ' '{print $5}' | awk '{ print substr( $0, 1, length($0)-1 ) }') -ge 80 ]]; then
+    echo -e "Partition: $x has exceeded over 80% utilization, please check partition"
+  fi
+done
+
