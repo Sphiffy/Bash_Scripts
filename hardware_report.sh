aac13cbf1e19d58c56a7d318e4e89660098ff9fb
Sphiffy <aitzazDX@yahoo.com>
2024-05-03 11:27:27 -0400

    Version 1.0 - Adding hardware metric variables

--- hardware_report.sh
+++ hardware_report.sh
@@ -5,5 +5,15 @@
 # Description: Script that is used to report hardware metrics on the current system
 
 
+# Variables
 
-#
+# Variable to fetch the CPU model using the /proc/cpuinfo file, awk to get first row, cut to show columns three and after
+cpu_model=$(cat /proc/cpuinfo | grep -i "^model name" | awk 'NR==1' | tr -s ' ' | cut -d ' ' -f 3-)
+
+# Variable to get current release
+current_release=$(cat /etc/redhat-release)
+
+# Variable to get current kernel version
+kernel_version=$(uname -r)
+
+
