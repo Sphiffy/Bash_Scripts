30e7076c928e86fd01bd315bba45dc0f569e704e
Sphiffy <aitzazDX@yahoo.com>
2024-05-03 12:16:47 -0400

    Version 1.2 - Adding root check and dump information blocks

--- hardware_report.sh
+++ hardware_report.sh
@@ -7,6 +7,9 @@
 
 # Variables
 
+# Variable for system hostname
+system_hostname=$(hostname)
+
 # Variable to fetch the CPU model using the /proc/cpuinfo file, awk to get first row, cut to show columns three and after
 cpu_model=$(cat /proc/cpuinfo | grep -i "^model name" | awk 'NR==1' | tr -s ' ' | cut -d ' ' -f 3-)
 
@@ -34,3 +37,38 @@ process_count=$(($total_processes - 1))
 block_devices=$(lsblk -dn)
 
 
+# Root check
+chk_root() {
+  local user_id=$(id -u)
+  if [[ $user_id -ne 0 ]]; then
+    echo "You need to be the root user to run this tool"
+    exit 999
+  fi
+}
+
+# Create the Report file
+REPORT="/tmp/hardware.$(date +'%d-%m-%y').info.txt"
+
+# Report Header
+report_header() {
+  echo -e "\n----------------------------------------"
+  echo -e "Generating Hardware Report"
+  echo -e "----------------------------------------\n"
+}
+
+# Information dump
+dump_info() {
+  echo -e "Hardware Report For: $system_hostname" > $REPORT
+  echo -e "----------------------------------------\n" >> $REPORT
+
+  # Current Release
+  echo -e "Release Version: $current_release" >> $REPORT
+  
+  # Current Kernel Version
+  echo -e "Current Kernel Version: $kernel_version" >> $REPORT
+
+}
+
+chk_root
+report_header
+dump_info
